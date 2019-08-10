92d6f26b8ea734071f647990f7b8ee29bc888114
Vuong Nguyen <yeungon@gmail.com>
2019-08-10 07:04:09 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,14 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <h1>
-      <marquee direction="right">Hello from Gatsby on Now 2.0</marquee>
+      <marquee direction="right">Hello from Gatsby on, are you sure?</marquee>
     </h1>
+  
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2, yes</Link>
+
   </Layout>
 )
 
