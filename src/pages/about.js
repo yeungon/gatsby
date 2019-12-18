5ed64819529926385b6554745a5985302216d69c
Vuong Nguyen <yeungon@gmail.com>
2019-12-19 03:29:12 +1300

    Add inline style

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
+import { heading, content } from '../style/inlinestyle'
+
 const About = () => {
   return (
     <Layout>
       <div>
-        <h1>About me!</h1>
+        <h1 style={heading}>About me!</h1>
         <br />
 
-        <p>
+        <p style={content}>
           In brief, Vuong is addicted to $addiction = []; $addiction["tech"] =
           "unlimited"; && addiction["philosophy"] = "analytic philosophy aka
           philosophy of language"; && $addiction ['literature'] = "short
@@ -17,12 +19,18 @@ const About = () => {
         </p>
 
         <br />
-        <p>
-          Fullstack developer, PHP framework creator, JAMstack lover (hint:
+        <p style={content}>
+          Fullstack developer, PHP framework creator, WordPress plugin builder, JAMstack lover (hint:
           Gatsby, GridSome): Experienced and work comfortably with, but not
           limited to, PHP, Laravel, WordPress, Codeigniter, (possibly Symfony ||
           Silverstripe || any PHP framework && CMS), MySQL, JS, jQuery, Vue.js,
           ReactJS, Bootstrap, HTML5, CSS. Certified elsewhere.
+        </p>
+        <br/>
+
+        <p style={content}>
+          I am a kind of perfectionism in everything especially web development!
+          To me, web should be easy to maintain, scale up but secured and a kind of art.
         </p>
       </div>
     </Layout>
